4b9f6704fb40300a4d2def3056ce1a3f965ce303
Patrick Goldinger <patrick@patrickgold.dev>
2025-08-11 14:27:30 +0200

    Add schemas module (#15)

---
+++ infra/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+readonly DEPLOY_USER="deploy"
+readonly CONTENT_DIR="/srv"
+
+if ! id "$DEPLOY_USER" &>/dev/null; then
+    useradd -m -s /bin/bash "$DEPLOY_USER"
+    echo "Deploy user created."
+fi
+
+mkdir -p "$CONTENT_DIR"
+chown -R "${DEPLOY_USER}:${DEPLOY_USER}" "$CONTENT_DIR"
+
+apt-get update
+apt-get -y install docker.io docker-compose fail2ban
+
+systemctl enable --now docker
+systemctl enable --now fail2ban
+
+docker-compose up -d
